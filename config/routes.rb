52168aa26c62489c1fa8a17a0b278519c31bde1a
Minh Vo <quangminhvo79@gmail.com>
2023-04-24 15:38:43 +0700

    chore & enhancement: Fix rubocop and config github actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get "users/:id", to: "users#show", as: :users
+  get 'users/:id', to: 'users#show', as: :users
   # root "articles#index"
 end
